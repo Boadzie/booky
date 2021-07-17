8f680e9b7598dd715c827b815fef8a55331cf823
Daniel Boadzie <boadziedaniel43@gmail.com>
2021-07-17 09:58:37 +0000

    add about page and static files

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import HomePageView
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('about/', TemplateView.as_view(template_name="about.html"), name="about"),
 ]
