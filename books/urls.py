3e2236780d4d94fd9b14aeb6054e2dc73d96cd63
Daniel Boadzie <boadziedaniel43@gmail.com>
2021-07-20 18:16:24 +0000

    add search form to books

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, SearchResultsListView
 
 urlpatterns = [
     path('', BookListView.as_view(), name="books"),
-    path('<uuid:pk>/', BookDetailView.as_view(), name="book")
+    path('<uuid:pk>/', BookDetailView.as_view(), name="book"),
+    path('search/', SearchResultsListView.as_view(), name='search_results')
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
@@ -16,3 +17,16 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = "books/book_detail.html"
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'search_results'
+    template_name = 'books/search_results.html'
+    #queryset = Book.objects.filter(title__icontains='Graphql')
+
+    def queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
